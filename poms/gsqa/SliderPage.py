109ca3a6340f6cd2a60e7f62c8928600190e0751
Ziryt <lef332@gmail.com>
2023-07-12 21:16:38 +0300

    slider fix

--- poms/gsqa/SliderPage.py
+++ poms/gsqa/SliderPage.py
@@ -10,7 +10,7 @@ class SliderPage:
     cp_button = Button(locator='Color Picker', by=By.ID)
     cp_text = Text(locator='//*[@aria-labelledby="tab_item-0"]/div')
     cp_iframe = Frame(locator='//*[contains(@data-src,"color")]')
-    cp_iframe_color = Container(locator='ui-widget-content ui-corner-all', by=By.CLASS_NAME)
+    cp_iframe_color = Container(locator='//*[@class="ui-widget-content ui-corner-all"]')
     cp_iframe_red = Container(locator='//*[@id="red"]/span')
     cp_iframe_green = Container(locator='//*[@id="green"]/span')
     cp_iframe_blue = Container(locator='//*[@id="blue"]/span')
@@ -30,6 +30,7 @@ class SliderPage:
     s_iframe = Frame(locator='//*[contains(@data-src,"step")]')
 
     def set_picker_color(self, red, green, blue):
-        self.cp_iframe_red.dragndrop(red, 0)
-        self.cp_iframe_green.dragndrop(green, 0)
-        self.cp_iframe_blue.dragndrop(blue, 0)
+        coefficient = 302 / 255
+        self.cp_iframe_red.dragndrop(red * coefficient, 0)
+        self.cp_iframe_green.dragndrop(green * coefficient, 0)
+        self.cp_iframe_blue.dragndrop(blue * coefficient, 0)
